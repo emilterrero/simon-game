386c2a5022e6b3143b8671b6bc835ee06a41b00a
Emil Terrero <emil@Emils-MacBook-Pro.local>
2022-03-15 16:15:33 -0400

    Async and await created, needs player turn now

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let contentCon = document.querySelector('.content-container')
 let gameTitle = document.querySelector('.game-title')
+let gameBtn = document.querySelector('.index-btn')
 let startBtn = document.querySelector('.start-btn')
+let resetBtn = document.querySelector('.reset-btn')
 let gameDiv = document.querySelector('.game-div')
 let gameBtns = document.querySelectorAll('.game-btn')
 let scoreboard = document.querySelector('.scoreboard')
@@ -13,27 +15,39 @@ let patternEvent = (time) => {
 }
 
 let startGame = () => {
-    let randomBtn = Math.floor(Math.random() * 4)
-    let btn = document.querySelector(`[data-id*='${randomBtn}']`)
-    gamePattern.push(randomBtn)
+    if (gamePattern.length == 0) {
+        let randomBtn = Math.floor(Math.random() * 4)
+        let btn = document.querySelector(`[data-id*='${randomBtn}']`)
+        gameBtn.classList.remove('start-btn')
+        gameBtn.classList.add('reset-btn')
+        gamePattern.push(randomBtn)
+        gameBtn.innerText = "Reset Game"
+        gameBtn.style.backgroundColor = "#ff6666"
 
-    let patternLoop = async () => {
-        for (let i = 0; i < gamePattern.length; i++) {
-            //let patternClick = () => {
-            //}
-            await patternEvent(1000)
-            console.log("pattern1")
-            gameBtns[gamePattern[i]].classList.add('pattern') 
-            //let patternUnclick = () => {
-            //}
-            await patternEvent(1000)
-            //console.log(gameBtns[gamePattern[i]]);
-            console.log("pattern2")
-            gameBtns[gamePattern[i]].classList.remove('pattern') 
+        let patternLoop = async () => {
+            for (let i = 0; i < gamePattern.length; i++) {
+                await patternEvent(1000)
+                gameBtns[gamePattern[i]].classList.add('pattern') 
+                await patternEvent(1000)
+                gameBtns[gamePattern[i]].classList.remove('pattern') 
+                await patternEvent(500)
+                for (let i = 0; i < gameBtns.length; i++) {
+                    let playerResponse = () => {
+                        console.log(gameBtns[i])
+                    }
+                    gameBtns[i].addEventListener('click', playerResponse)
+                }
+            }
         }
-        //console.log(gamePattern)
+        patternLoop();
+        console.log(gameBtns)
+    } else {
+        gameBtn.innerText = "Start Game"
+        gameBtn.style.backgroundColor = 'lightblue'
+        gamePattern = []
+        playerPattern = []
+        countIndex = 0
     }
-    patternLoop();
 }
 
 startBtn.addEventListener('click', startGame)
